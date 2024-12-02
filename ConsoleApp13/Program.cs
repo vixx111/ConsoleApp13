75f057d4749ab737e4ffa375e1952fa892e25e92
Admin <Admin@DESKTOP-UGP0C2G>
2024-12-02 13:56:52 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,38 @@
+﻿int n = 7;
+int m = 7;
+double[,] matrix = new double[n, m];
+Random rand = new Random();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        matrix[i, j] = rand.NextDouble() * 20 - 10;
+    }
+}
+Console.WriteLine("Сгенерированная матрица:");
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        Console.Write($"{matrix[i, j]:0.00}\t");
+    }
+    Console.WriteLine();
+}
+double product = 1;
+bool hasPositive = false; 
+for (int i = 1; i < n; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        if (matrix[i, j] > 0)
+        {
+            product *= matrix[i, j];
+            hasPositive = true;
+        }
+    }
+}
+if (!hasPositive)
+{
+    product = 0;
+}
+Console.WriteLine($"Произведение положительных элементов под главной диагональю: {product}");
